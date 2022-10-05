d72c6d25b69ef190a363a98a9637cfc5545dbdef
Donadominic <43172188+Donadominic@users.noreply.github.com>
2022-10-05 11:40:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
+class A {
+  String meth() {
+    String str="Invoking method from class A";
+    return str;
+  }
+}
 
+class B extends A {
+  String meth() {
+    String str="Method is overridden in Extendend class B";
+    return str;
+  }
+}
 public class Main {
-  
+  public static void main(String[]args) {
+    B b1=new B();
+    b.meth();
+  }
 }
